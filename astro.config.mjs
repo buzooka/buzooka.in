96e53fd9a8598104a73dcdcab652ae2b3ece8a38
Vishnu <vishnu88sankaran@gmail.com>
2024-10-17 17:25:51 +0530

    Workflow changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://buzooka.in',
     integrations: [svelte(), tailwind({
         applyBaseStyles: false,
     })]
